8360025082e03185a174030c684c6b620fda4dde
gscerveira <61796134+gscerveira@users.noreply.github.com>
2024-09-13 17:39:33 -0300

    Repositórios de User e Roteiro

---
+++ roteiros/src/main/java/com/cooperfilme/roteiros/repository/RoteiroRepository.java
@@ -0,0 +1,24 @@
+package com.cooperfilme.roteiros.repository;
+
+import com.cooperfilme.roteiros.model.Roteiro;
+import com.cooperfilme.roteiros.model.RoteiroStatus;
+
+import org.springframework.cglib.core.Local;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+@Repository
+public interface RoteiroRepository extends JpaRepository<Roteiro, Long> {
+    List<Roteiro> findByStatus(RoteiroStatus status);
+    List<Roteiro> findByClientEmail(String clientEmail);
+    List<Roteiro> findByCreatedAtBetween(LocalDateTime start, LocalDateTime end);
+    List<Roteiro> findByStatusAndCreatedAtBetweenAndClientEmail(
+        RoteiroStatus status,
+        LocalDateTime start,
+        LocalDateTime end,
+        String clientEmail
+    );
+}
