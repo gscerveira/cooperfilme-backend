8431ad1d25b2de5f29457792c43e720fe5f82e77
gscerveira <61796134+gscerveira@users.noreply.github.com>
2024-09-12 22:41:22 -0300

    Setup inicial

---
+++ roteiros/src/main/java/com/cooperfilme/roteiros/RoteirosApplication.java
@@ -0,0 +1,13 @@
+package com.cooperfilme.roteiros;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RoteirosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RoteirosApplication.class, args);
+	}
+
+}
---
+++ roteiros/src/test/java/com/cooperfilme/roteiros/RoteirosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cooperfilme.roteiros;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RoteirosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
